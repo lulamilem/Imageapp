9c955d00275775675bbdf5e1335ab572c0011430
lulamilem <mkhungela.l@gmail.com>
2017-11-20 11:07:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/admin/imageapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.admin.imageapp;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    activity_swipe activity_swipe;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager=(ViewPager)findViewById(R.id.viewPager);
+        activity_swipe =new activity_swipe(this);
+        viewPager.setAdapter(activity_swipe);
+    }
+}
+
---
+++ app/src/main/java/com/example/admin/imageapp/activity_swipe.java
@@ -0,0 +1,52 @@
+package com.example.admin.imageapp;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Torab on 20-May-16.
+ */
+public class activity_swipe extends PagerAdapter {
+    private int [] imageResources ={R.drawable.capture1,R.drawable.capture2,R.drawable.capture3,R.drawable.capture4,R.drawable.capture5};
+    private Context ctx;
+    private LayoutInflater layoutInflater;
+
+    public activity_swipe(Context c) {
+        ctx=c;
+    }
+
+    @Override
+    public int getCount() {
+
+        return imageResources.length;
+    }
+
+
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater= (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView=layoutInflater.inflate(R.layout.activity_swipe,container,false);
+        ImageView imageView=(ImageView) itemView.findViewById(R.id.swip_image_view);
+        imageView.setImageResource(imageResources[position]);
+        container.addView(itemView);
+        return itemView;
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+
+        return  (view==object);
+    }
+}
